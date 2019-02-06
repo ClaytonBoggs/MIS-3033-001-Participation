fab3675e1ce86b58512f0c86dd9a87a2253f9349
Clayton Boggs <cboggs2015@hotmail.com>
2019-02-06 11:46:22 -0600

    First draft of WPF1

---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Form> forms = new List<Form>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Form individual = new Form();
+            individual.Name = nameBox.Text;
+            individual.Address = addressBox.Text;
+            individual.ZipCode = Convert.ToInt32(zipBox.Text);
+            list.Items.Add(nameBox.Text);
+            list.Items.Add(addressBox.Text);
+            list.Items.Add(zipBox.Text);
+        }
+    }
+}
